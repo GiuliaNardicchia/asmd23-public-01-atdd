b4704625d2eb3ad7304f040ce66d60ed41caf06c
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2024-02-23 12:16:59 +0100

    refactor code

--- src/test/java/calculator/CalculatorSteps.java
+++ src/test/java/calculator/CalculatorSteps.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.AfterAll;
 
 public class CalculatorSteps {
     private int res = 0;
@@ -11,46 +12,47 @@ public class CalculatorSteps {
         this.calculator = new Calculator();
     }
 
-    @When("I add {int} and {int}")
-    public void iAddAnd(int arg0, int arg1) {
+    private void checkResult(int arg0) {
+        if (arg0 != this.calculator.getResult()) { // or using Junit's asserts
+            throw new IllegalStateException();
+        }
+    }
+    private void enterTwoNumbers(int arg0, int arg1) {
         this.calculator.enter(arg0);
         this.calculator.enter(arg1);
+    }
+
+    @When("I add {int} and {int}")
+    public void iAddAnd(int arg0, int arg1) {
+        enterTwoNumbers(arg0, arg1);
     }
 
     @Then("the sum should be {int}")
     public void theSumShouldBe(int arg0) {
         this.calculator.add();
-        if (arg0 != this.calculator.getResult()) { // or using Junit's asserts
-            throw new IllegalStateException();
-        }
+        checkResult(arg0);
     }
 
     @When("I subtract {int} and {int}")
     public void iSubtractAnd(int arg0, int arg1) {
-        this.calculator.enter(arg0);
-        this.calculator.enter(arg1);
+        enterTwoNumbers(arg0, arg1);
     }
 
     @Then("the difference should be {int}")
     public void theDifferenceShouldBe(int arg0) {
         this.calculator.subtract();
-        if (arg0 != this.calculator.getResult()) {
-            throw new IllegalStateException();
-        }
+        checkResult(arg0);
     }
 
     @When("I multiply {int} and {int}")
     public void iMultiplyAnd(int arg0, int arg1) {
-        this.calculator.enter(arg0);
-        this.calculator.enter(arg1);
+        enterTwoNumbers(arg0, arg1);
     }
 
     @Then("the product should be {int}")
     public void theProductShouldBe(int arg0) {
         this.calculator.multiply();
-        if (arg0 != this.calculator.getResult()) {
-            throw new IllegalStateException();
-        }
+        checkResult(arg0);
     }
 
 }
